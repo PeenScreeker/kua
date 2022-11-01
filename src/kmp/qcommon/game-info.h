a7cad494b866cc0a5e48d987b61c51ac9284c883
sOkam! <itsfatum@gmail.com>
2022-11-01 22:02:15 +0100

    new: Updater/Launcher app

---
+++ src/kmp/qcommon/game-info.h
@@ -0,0 +1,10 @@
+//.......................................
+#define E_FULLNAME "Kua: Engine"
+#define E_VERSION  "1.33a"
+//.......................................
+#define G_SHORTNAME  "kmp"
+#define G_FULLNAME   "Kua: Multiplayer"
+#define G_VERSION    "0.35a"
+#define G_LICENSE    "GPL3 or higher"
+#define SEP          " "
+//.......................................
